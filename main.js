75972ec3cb2af620703234605f96d3518d01634a
FadilMS18 <muhfadilms2418@gmail.com>
2025-10-31 20:38:50 +0800

    Finished the sticky navbar
    added the logic for sticky navbar
    every time user scroll past 100 point, the sticky navbar will come out
    tidy the css code a bit by grouping the same write css code
    
    also change the p tag on the navbar to anchor tag for easier navigation

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 console.log('Hello, world!!!');
 
 let navbar1 = document.querySelector("#nav1")
+let stickyNav = document.querySelector("#sticky-nav")
 function time(){
     setTimeout(()=>{
-        navbar1.classList.remove("disappear")
+        navbar1.classList.remove("nav-start-disappear")
     },150)
 }
 
+function stickyNavToggle(){
+    if(window.scrollY >= 100){
+        if(stickyNav.classList.contains("sticky-nav-disappear")){
+            stickyNav.classList.remove("sticky-nav-disappear")
+        }
+    }else{
+        stickyNav.classList.add("sticky-nav-disappear")
+    }
+}
 
-// document.addEventListener("DOMContentLoaded", time)
+document.addEventListener("scroll", stickyNavToggle)
+document.addEventListener("DOMContentLoaded", time)
 
