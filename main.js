3d2965b701fa952dafca8d8e60d0d5fbf6b6b18c
FadilMS18 <muhfadilms2418@gmail.com>
2025-10-31 17:33:10 +0800

    added sticky navbar
    added sticky navbar that will come out when user scroll past the navbar

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Hello, world!!!');
+
+let navbar1 = document.querySelector("#nav1")
+function time(){
+    setTimeout(()=>{
+        navbar1.classList.remove("disappear")
+    },150)
+}
+
+
+// document.addEventListener("DOMContentLoaded", time)
+
