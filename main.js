1f3e8c2f55695b63f2a6586afe2b9db3b0979730
FadilMS18 <muhfadilms2418@gmail.com>
2025-10-31 23:32:30 +0800

    Finished sidebar & container
    added sidebar & container
    both using absolute position to make it easier to place
    the side bar is fixed position so we that will stick the page
    even if we scroll

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ console.log('Hello, world!!!');
 
 let navbar1 = document.querySelector("#nav1")
 let stickyNav = document.querySelector("#sticky-nav")
+let catalog = document.querySelector("#sidebar > h3")
 function time(){
     setTimeout(()=>{
         navbar1.classList.remove("nav-start-disappear")
-    },150)
+    },5)
 }
 
 function stickyNavToggle(){
     if(window.scrollY >= 100){
         if(stickyNav.classList.contains("sticky-nav-disappear")){
             stickyNav.classList.remove("sticky-nav-disappear")
+            catalog.classList.add("nav-start-disappear")
         }
     }else{
         stickyNav.classList.add("sticky-nav-disappear")
+        catalog.classList.remove("nav-start-disappear")
     }
 }
 
