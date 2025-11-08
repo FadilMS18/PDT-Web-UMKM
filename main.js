33ef1865312714073673a4e22fca65bee3545018
FadilMS18 <muhfadilms2418@gmail.com>
2025-11-08 11:34:12 +0800

    Added Catalog Content & About Me Content
    Added about me content & container
    added content catalog content card
    change the heading text with outfit font

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-console.log('Hello, world!!!');
-
 let navbar1 = document.querySelector("#nav1")
 let stickyNav = document.querySelector("#sticky-nav")
 let catalog = document.querySelector("#sidebar > h3")
+
 function time(){
     setTimeout(()=>{
         navbar1.classList.remove("nav-start-disappear")
     },5)
 }
 
+console.log(catalog)
 function stickyNavToggle(){
-    if(window.scrollY >= 100){
+    if(window.scrollY >= 40){
         if(stickyNav.classList.contains("sticky-nav-disappear")){
             stickyNav.classList.remove("sticky-nav-disappear")
             catalog.classList.add("nav-start-disappear")
