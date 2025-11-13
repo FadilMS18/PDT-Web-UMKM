eb30c96da460442e523d680831b9d8e2ab256418
FadilMS18 <muhfadilms2418@gmail.com>
2025-11-13 23:54:24 +0800

    fixed card title that disappear on hover
    now the card title will stay in place
    but category and price will be disappear on hover

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-let navbar1 = document.querySelector("#nav1")
-let stickyNav = document.querySelector("#sticky-nav")
-let catalog = document.querySelector("#sidebar > h3")
-let prevScrollPos = window.pageYOffset
+let navbar1 = document.querySelector("#nav1");
+let stickyNav = document.querySelector("#sticky-nav");
+let catalog = document.querySelector("#sidebar > h3");
+let prevScrollPos = window.pageYOffset;
 
-function time(){
-    setTimeout(()=>{
-        navbar1.classList.remove("nav-start-disappear")
-    },5)
+function time() {
+  setTimeout(() => {
+    navbar1.classList.remove("nav-start-disappear");
+  }, 5);
 }
 
-console.log(catalog)
+console.log(catalog);
 
-function stickyNavbarShow(){
-    const currentScrollPos = window.pageYOffset;
-    if(prevScrollPos > currentScrollPos){
-        stickyNav.classList.remove("sticky-nav-disappear")
-    }else{
-        stickyNav.classList.add("sticky-nav-disappear")
-    }
-    prevScrollPos = currentScrollPos
-    if(window.pageYOffset <= 40){
-        stickyNav.classList.add("sticky-nav-disappear")
-    }
+function stickyNavbarShow() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    stickyNav.classList.remove("sticky-nav-disappear");
+  } else {
+    stickyNav.classList.add("sticky-nav-disappear");
+  }
+  prevScrollPos = currentScrollPos;
+  if (window.pageYOffset <= 40) {
+    stickyNav.classList.add("sticky-nav-disappear");
+  }
 }
-document.addEventListener("scroll", stickyNavbarShow)
-document.addEventListener("DOMContentLoaded", time)
+document.addEventListener("scroll", stickyNavbarShow);
+document.addEventListener("DOMContentLoaded", time);
 
-const catalogCard = Array.from(document.querySelectorAll(".catalog-card"))
-catalogCard.forEach(card => {
-    card.addEventListener("mouseover", cardDescDisappear)
-    card.addEventListener("mouseout", cardDescAppear)
+const catalogCard = Array.from(document.querySelectorAll(".catalog-card"));
+catalogCard.forEach((card) => {
+  const kategori = document.querySelector("#catalog-card-kategori-makanan");
+  const harga = document.querySelector("#catalog-card-harga-makanan");
+  card.addEventListener("mouseover", () => cardDescDisappear(kategori, harga));
+  card.addEventListener("mouseout", () => cardDescAppear(kategori, harga));
 });
 
-function cardDescDisappear(){
-    let descContainer = this.querySelector(".desc-container")
-    descContainer.classList.add("catalog-description-disappear")
+function cardDescDisappear(element1, element2) {
+  element1.classList.add("catalog-description-disappear");
+  element2.classList.add("catalog-description-disappear");
 }
 
-function cardDescAppear(){
-    let descContainer = this.querySelector(".desc-container")
-    if(descContainer.classList.contains("catalog-description-disappear")){
-        descContainer.classList.remove("catalog-description-disappear")
-    }
+function cardDescAppear(element1, element2) {
+  if (
+    element1.classList.contains("catalog-description-disappear") &&
+    element2.classList.contains("catalog-description-disappear")
+  ) {
+    element1.classList.remove("catalog-description-disappear");
+    element2.classList.remove("catalog-description-disappear");
+  }
 }
