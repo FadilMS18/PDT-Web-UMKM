4c859a5a355aefdd067ae277ce72aafe5f0f6310
FadilMS18 <muhfadilms2418@gmail.com>
2025-11-16 01:44:52 +0800

    Fix Some Issue in the navbar logic
    move sticky navbar login back to main.js files
    so that the index page can use the sticky navbar as well

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 const navbar1 = document.querySelector("#nav1");
+const stickyNav = document.querySelector("#sticky-nav");
+let prevScrollPos = window.pageYOffset;
+
+// Sticky navbar akan muncul jika kita mengscroll keatas dan hilkang kalau scroll kebawah
+function stickyNavbarShow() {
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    stickyNav.classList.remove("sticky-nav-disappear");
+  } else {
+    stickyNav.classList.add("sticky-nav-disappear");
+  }
+  prevScrollPos = currentScrollPos;
+  if (window.pageYOffset <= 40) {
+    stickyNav.classList.add("sticky-nav-disappear");
+  }
+}
+
+document.addEventListener("scroll", stickyNavbarShow);
+
 
 // Function untuk membuat animasi navbar pas page pertama direload
 function time() {
